cbaa96a74f7f08090f5da7c165a27cb3f5936b9b
ignaciosy <ignacio_sy@hotmail.com>
2018-06-05 18:36:03 -0400

    asd

--- pixz_interno/pixz_interno/settings.py
+++ pixz_interno/pixz_interno/settings.py
@@ -82,8 +82,9 @@ WSGI_APPLICATION = 'pixz_interno.wsgi.application'
 #}
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
+#DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
 
 
 # Password validation
