c2fcbcaf642d258a68a7e145df9a76443bbd7724
ignaciosy <ignacio_sy@hotmail.com>
2018-06-05 10:41:37 -0400

    Removed virtual from repository

--- pixz_interno/virtual/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\ina\dropbox\ina\_programacion\weddi\pixz_interno\interno\pixz_interno\virtual\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
