dd3e683bb3bb253a85991c827a8eb3a6546432ac
ignaciosy <ignacio_sy@hotmail.com>
2018-06-05 18:32:00 -0400

    asd

--- pixz_interno/pixz_interno/settings.py
+++ pixz_interno/pixz_interno/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'pixz_interno.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
