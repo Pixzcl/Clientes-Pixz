0ed5b497d35de9d93fd2287587d50f4bef4e127b
ignaciosy <ignacio_sy@hotmail.com>
2018-07-11 20:33:19 -0400

    portada y errores

--- pixz_interno/Eventos/views.py
+++ pixz_interno/Eventos/views.py
@@ -55,13 +55,20 @@ def index(request):
 		"s3": satisfaccion[2].count(),
 		"s2": satisfaccion[3].count(),
 		"s1": satisfaccion[4].count(),
+	}
+	if evaluados == 0:
+		"sp5": 0,
+		"sp4": 0,
+		"sp3": 0,
+		"sp2": 0,
+		"sp1": 0,
+	else:
 		"sp5": "%.2f" % (float(satisfaccion[0].count())/evaluados * 100),
 		"sp4": "%.2f" % (float(satisfaccion[1].count())/evaluados * 100),
 		"sp3": "%.2f" % (float(satisfaccion[2].count())/evaluados * 100),
 		"sp2": "%.2f" % (float(satisfaccion[3].count())/evaluados * 100),
 		"sp1": "%.2f" % (float(satisfaccion[4].count())/evaluados * 100),
 
-	}
 	return render(request, 'index.html', context)
 
 
