ec98dbc25890e39ea8b9d9c62a6456d829f46717
ignaciosy <ignaciosy0@gmail.com>
2018-09-30 01:40:50 -0300

    login ventana, logout, password

--- pixz_interno/pixz_interno/urls.py
+++ pixz_interno/pixz_interno/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-	url(r'^login/$', auth_views.login, {'template_name': 'login1.html'}, name='login'),
+	url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '../login/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^no_autorizado/', views.no_autorizado, name='no_autorizado'),
